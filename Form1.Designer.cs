3bc7614ae6122140db06b7baad606c37ec38814c
b98b09 <b98b09@yahoo.co.jp>
2026-05-18 23:14:10 +0900

    パーセントエンコーディング、デコード側も追加。 サイズ微調整。

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,6 +36,12 @@
             splitContainer1 = new SplitContainer();
             tabControl1 = new TabControl();
             tabPage1 = new TabPage();
+            checkBoxTab1AutoCopy = new CheckBox();
+            groupBox2 = new GroupBox();
+            textBoxTab1DecOutput = new TextBox();
+            label2 = new Label();
+            textBoxTab1DecInput = new TextBox();
+            label1 = new Label();
             groupBox1 = new GroupBox();
             textBoxTab1Output = new TextBox();
             labelTab1Output = new Label();
@@ -49,6 +55,7 @@
             splitContainer1.SuspendLayout();
             tabControl1.SuspendLayout();
             tabPage1.SuspendLayout();
+            groupBox2.SuspendLayout();
             groupBox1.SuspendLayout();
             SuspendLayout();
             // 
@@ -96,6 +103,7 @@
             // splitContainer1
             // 
             splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.FixedPanel = FixedPanel.Panel1;
             splitContainer1.Location = new Point(0, 0);
             splitContainer1.Name = "splitContainer1";
             // 
@@ -103,7 +111,7 @@
             // 
             splitContainer1.Panel2.Controls.Add(tabControl1);
             splitContainer1.Size = new Size(800, 400);
-            splitContainer1.SplitterDistance = 266;
+            splitContainer1.SplitterDistance = 44;
             splitContainer1.TabIndex = 0;
             // 
             // tabControl1
@@ -114,38 +122,102 @@
             tabControl1.Location = new Point(0, 0);
             tabControl1.Name = "tabControl1";
             tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(530, 400);
+            tabControl1.Size = new Size(752, 400);
             tabControl1.TabIndex = 0;
             // 
             // tabPage1
             // 
+            tabPage1.Controls.Add(checkBoxTab1AutoCopy);
+            tabPage1.Controls.Add(groupBox2);
             tabPage1.Controls.Add(groupBox1);
             tabPage1.Location = new Point(4, 29);
             tabPage1.Name = "tabPage1";
             tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(522, 367);
+            tabPage1.Size = new Size(744, 367);
             tabPage1.TabIndex = 0;
             tabPage1.Text = "tabPage1";
             tabPage1.UseVisualStyleBackColor = true;
             // 
+            // checkBoxTab1AutoCopy
+            // 
+            checkBoxTab1AutoCopy.AutoSize = true;
+            checkBoxTab1AutoCopy.Checked = true;
+            checkBoxTab1AutoCopy.CheckState = CheckState.Checked;
+            checkBoxTab1AutoCopy.Location = new Point(12, 185);
+            checkBoxTab1AutoCopy.Name = "checkBoxTab1AutoCopy";
+            checkBoxTab1AutoCopy.Size = new Size(409, 24);
+            checkBoxTab1AutoCopy.TabIndex = 4;
+            checkBoxTab1AutoCopy.Text = "自動コピー（エンコード側の出力をデコード側の入力にコピーする）";
+            checkBoxTab1AutoCopy.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            groupBox2.Controls.Add(textBoxTab1DecOutput);
+            groupBox2.Controls.Add(label2);
+            groupBox2.Controls.Add(textBoxTab1DecInput);
+            groupBox2.Controls.Add(label1);
+            groupBox2.Location = new Point(6, 215);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(730, 146);
+            groupBox2.TabIndex = 3;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "デコード";
+            // 
+            // textBoxTab1DecOutput
+            // 
+            textBoxTab1DecOutput.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            textBoxTab1DecOutput.Location = new Point(6, 99);
+            textBoxTab1DecOutput.Name = "textBoxTab1DecOutput";
+            textBoxTab1DecOutput.Size = new Size(718, 27);
+            textBoxTab1DecOutput.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(6, 76);
+            label2.Name = "label2";
+            label2.Size = new Size(53, 20);
+            label2.TabIndex = 2;
+            label2.Text = "output";
+            // 
+            // textBoxTab1DecInput
+            // 
+            textBoxTab1DecInput.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            textBoxTab1DecInput.Location = new Point(6, 46);
+            textBoxTab1DecInput.Name = "textBoxTab1DecInput";
+            textBoxTab1DecInput.Size = new Size(718, 27);
+            textBoxTab1DecInput.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(6, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(43, 20);
+            label1.TabIndex = 0;
+            label1.Text = "input";
+            // 
             // groupBox1
             // 
+            groupBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             groupBox1.Controls.Add(textBoxTab1Output);
             groupBox1.Controls.Add(labelTab1Output);
             groupBox1.Controls.Add(labelTab1Input);
             groupBox1.Controls.Add(textBoxTab1Input);
             groupBox1.Location = new Point(6, 6);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(508, 206);
+            groupBox1.Size = new Size(730, 145);
             groupBox1.TabIndex = 2;
             groupBox1.TabStop = false;
             groupBox1.Text = "パーセントエンコーディング";
             // 
             // textBoxTab1Output
             // 
+            textBoxTab1Output.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             textBoxTab1Output.Location = new Point(6, 99);
             textBoxTab1Output.Name = "textBoxTab1Output";
-            textBoxTab1Output.Size = new Size(496, 27);
+            textBoxTab1Output.Size = new Size(718, 27);
             textBoxTab1Output.TabIndex = 3;
             // 
             // labelTab1Output
@@ -168,9 +240,10 @@
             // 
             // textBoxTab1Input
             // 
+            textBoxTab1Input.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             textBoxTab1Input.Location = new Point(6, 46);
             textBoxTab1Input.Name = "textBoxTab1Input";
-            textBoxTab1Input.Size = new Size(496, 27);
+            textBoxTab1Input.Size = new Size(718, 27);
             textBoxTab1Input.TabIndex = 0;
             // 
             // tabPage2
@@ -178,7 +251,7 @@
             tabPage2.Location = new Point(4, 29);
             tabPage2.Name = "tabPage2";
             tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(522, 367);
+            tabPage2.Size = new Size(763, 367);
             tabPage2.TabIndex = 1;
             tabPage2.Text = "tabPage2";
             tabPage2.UseVisualStyleBackColor = true;
@@ -202,6 +275,9 @@
             splitContainer1.ResumeLayout(false);
             tabControl1.ResumeLayout(false);
             tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
             ResumeLayout(false);
@@ -224,5 +300,11 @@
         private Label labelTab1Input;
         private TextBox textBoxTab1Input;
         private TabPage tabPage2;
+        private GroupBox groupBox2;
+        private TextBox textBoxTab1DecOutput;
+        private Label label2;
+        private TextBox textBoxTab1DecInput;
+        private Label label1;
+        private CheckBox checkBoxTab1AutoCopy;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -9,6 +9,16 @@ namespace MiscTest
             // Tab1
             textBoxTab1Input.TextChanged += TextBoxTab1Input_TextChanged;
             textBoxTab1Output.ReadOnly = true;
+
+            textBoxTab1DecInput.TextChanged += TextBoxTab1DecInput_TextChanged;
+            textBoxTab1DecOutput.ReadOnly = true;
+        }
+
+        private void TextBoxTab1DecInput_TextChanged(object? sender, EventArgs e)
+        {
+            string input = textBoxTab1DecInput.Text;
+            string output = Uri.UnescapeDataString(input);
+            textBoxTab1DecOutput.Text = output;
         }
 
         private void TextBoxTab1Input_TextChanged(object? sender, EventArgs e)
@@ -16,6 +26,10 @@ namespace MiscTest
             string input = textBoxTab1Input.Text;
             string output = Uri.EscapeDataString(input);
             textBoxTab1Output.Text = output;
+            if (checkBoxTab1AutoCopy.Checked)
+            {
+                textBoxTab1DecInput.Text = output;
+            }
         }
     }
 }
