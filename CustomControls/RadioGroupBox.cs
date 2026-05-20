eb4b09185a4e612dcadfa805f0ba1b820aa40544
b98b09 <b98b09@yahoo.co.jp>
2026-05-20 21:50:36 +0900

    RadioGroupBoxを作成。
    GroupBox内にRadioButton並べて、チェック状態の変化イベントを一本化できる。

---
+++ CustomControls/RadioGroupBox.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiscTest.CustomControls
+{
+    /// <summary>
+    /// RadioButton専用のGroupBox。
+    /// GroupBox内にFlowLayoutPanelを配置してRadioButtonを縦に並べることができます。
+    /// </summary>
+    public class RadioGroupBox : GroupBox
+    {
+        /// <summary>
+        /// RadioButtonを縦に並べるためにFlowLayoutPanelを使用します。
+        /// </summary>
+        private readonly FlowLayoutPanel flowLayoutPanel;
+
+        /// <summary>
+        /// RadioButtonのCheckChangedイベントを橋渡しするイベント。
+        /// </summary>
+        public event EventHandler? CheckedChanged;
+
+        /// <summary>
+        /// RadioButton専用のGroupBoxを作ります。
+        /// </summary>
+        public RadioGroupBox()
+        {
+            flowLayoutPanel = new FlowLayoutPanel
+            {
+                Dock = DockStyle.Fill,
+                AutoSize = true,
+                AutoSizeMode = AutoSizeMode.GrowAndShrink,
+                FlowDirection = FlowDirection.TopDown,
+                WrapContents = false
+            };
+            Controls.Add(flowLayoutPanel);
+        }
+
+        /// <summary>
+        /// RadioButtonを追加する。
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="tag"></param>
+        public void AddItem(string text, object? tag = null)
+        {
+            var rb = new RadioButton
+            {
+                Text = text,
+                Tag = tag
+            };
+            rb.CheckedChanged += (s, e) =>
+            {
+                CheckedChanged?.Invoke(s, e);
+            };
+            flowLayoutPanel.Controls.Add(rb);
+        }
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,6 @@
+using MiscTest.CustomControls;
+using System.Diagnostics;
+
 namespace MiscTest
 {
     public partial class Form1 : Form
@@ -12,6 +15,39 @@ namespace MiscTest
 
             textBoxTab1DecInput.TextChanged += TextBoxTab1DecInput_TextChanged;
             textBoxTab1DecOutput.ReadOnly = true;
+
+            this.Load += Form1_Load;
+            this.Shown += Form1_Shown;
+            this.FormClosed += Form1_FormClosed;
+        }
+
+
+
+
+
+        private void Form1_Load(object? sender, EventArgs e)
+        {
+            RadioGroupBox rBox = new();
+            rBox.AddItem("Option 1", 1);
+            rBox.AddItem("Option 2", 2);
+            rBox.AddItem("Option 3", 3);
+            rBox.CheckedChanged += (sender, e) =>
+            {
+                if (sender is RadioButton rb)
+                {
+                    Debug.WriteLine($"Selected: {rb.Tag}, Checked: {rb.Checked}");
+                }
+            };
+            tabPage2.Controls.Add(rBox);
+        }
+
+        private void Form1_Shown(object? sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+        private void Form1_FormClosed(object? sender, FormClosedEventArgs e)
+        {
+            //throw new NotImplementedException();
         }
 
         private void TextBoxTab1DecInput_TextChanged(object? sender, EventArgs e)
